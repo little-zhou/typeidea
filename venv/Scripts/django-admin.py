201e2dc01a966dfea4bc9631852b069ceaf7c076
little-zhou <424048725@qq.com>
2021-08-18 07:40:16 +0800

    初始化提交

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\program files\jetbrains\pycharmprojects\typeidea\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
